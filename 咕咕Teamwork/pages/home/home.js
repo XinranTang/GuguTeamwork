98bdc165f7b1789b58a8cfa9515adc0b2fe504d1
Aka <784595821@qq.com>
2019-04-18 11:11:01 +0800

    解决了日程表不能根据时间改变的bug

--- 咕咕Teamwork/pages/home/home.js
+++ 咕咕Teamwork/pages/home/home.js
@@ -20,6 +20,7 @@ Page({
       hour:0,
       weekDay:"",
   },
+    dateNow:'',
     color:{},
     colorArr: ["#ff6666", "#cc6666", "#ffcc99", "#6699ff", "#ff9966",
       "#99cc99", "#ff9999", "#99cccc", "#cc99cc", "#99cccc", "#cc9999",
@@ -42,14 +43,26 @@ Page({
    */
   onLoad: function (options) {
     var self = this
+    var date = new Date()
+    var sdate =""+ date.getFullYear()+""+(date.getMonth()+1)+""+date.getDate()
+    self.setData({
+      dateNow:sdate
+    })
     wx.getStorage({
       key: 'Schedule',
       success: function (res) {
-        console.log(res.data)
+        // console.log(res.data)
         if(res.data!=null){
-          self.setData({
-            list: res.data
+          res.data.forEach(each=>{
+            if(each.Date==sdate){
+              self.setData({
+                list:each.List
+              })
+            }
           })
+          // self.setData({
+          //   list: res.data
+          // })
         }
       },
       fail:function(res){
@@ -96,8 +109,8 @@ Page({
     let colorArr = self.data.colorArr;
     let colorLen = colorArr.length;
     let randomColorArr = [];
-    console.log("本地任务数量")
-    console.log(self.data.tasks.length)
+    // console.log("本地任务数量")
+    // console.log(self.data.tasks.length)
     //判断执行
     do {
       let random = colorArr[Math.floor(Math.random() * colorLen)];
@@ -118,23 +131,80 @@ Page({
     self.setData({
       randomColorArrT: randomColorArr
     });
-    console.log(randomColorArr)
+    // console.log(randomColorArr)
   },
 
   /**
    * Lifecycle function--Called when page hide
    */
   onHide: function () {
+    let self = this;
+    let arr = this.data.list;
+    arr.forEach(each=>{
+      if(each.choosen){
+        each.choosen = false;
+        each.color="#eeeeee";
+      }
+    })
+    self.setData({
+      list:arr
+    })
+    var obj = wx.getStorage({
+      key: 'Schedule',
+      success: function (res) {
+        res.data.forEach(each => {
+          if (each.Date == self.data.dateNow) {
+            each.List = self.data.list;
+            each.List.forEach(item => {
+              if (item.choosen == true) {
+                item.choosen = false;
+              }
+            })
+            wx.setStorage({
+              key: 'Schedule',
+              data: res.data,
+            })
+          }
+        })
+      },
+      fail: function () {
+        wx.setStorage({
+          key: 'Schedule',
+          data: [{ "Date": self.data.dateNow, 'List': self.data.list },],
+        })
+      }
+    })
   },
 
   /**
    * Lifecycle function--Called when page unload
    */
   onUnload: function () {
-    var self = this
-    wx.setStorage({
+    var self = this;
+    var obj = wx.getStorage({
       key: 'Schedule',
-      data: self.data.list,
+      success: function(res) {
+        res.data.forEach(each=>{
+          if(each.Date==self.data.dateNow){
+            each.List = self.data.list;
+            each.List.forEach(item=>{
+              if(item.choosen==true){
+                item.choosen = false;
+              }
+            })
+            wx.setStorage({
+              key: 'Schedule',
+              data: res.data,
+            })
+          }
+        })
+      },
+      fail:function(){
+        wx.setStorage({
+          key: 'Schedule',
+          data: [{"Date":self.data.dateNow,'List':self.data.list},],
+        })
+      }
     })
   },
 
@@ -159,17 +229,101 @@ Page({
 
   },
   DateChange(e) {
+    var self = this;
     let show_day = new Array('周日', '周一', '周二', '周三', '周四', '周五', '周六');
     var format = e.detail.value.replace(/-/g, '/')
     var countDown =new Date(format)
     var wd = show_day[countDown.getDay()]
     var tmonth = e.detail.value[5] == '0' ? e.detail.value.slice(6, 7) + "月" : e.detail.value.slice(5, 7) + "月"
-    this.setData({
-      schedule:{
-        year: e.detail.value.slice(0, 4),
-        month:tmonth,
-        day:e.detail.value.slice(8,10),
-        weekDay:wd
+    wx.getStorage({
+      key: 'Schedule',
+      success: function(res) {
+        // console.log("现在日期" + (e.detail.value.slice(0, 4) + "" + (e.detail.value[5] == '0' ? e.detail.value.slice(6, 7) : e.detail.value.slice(5, 7)) + "" + e.detail.value.slice(8, 10)))
+        var flag = false;
+        res.data.forEach(each=>{
+          if(each.Date==self.data.dateNow){
+            // console.log("对应")
+            each.List = self.data.list
+            flag=true;
+          }
+        })
+        if(!flag){
+          // console.log("push当前"+self.data.dateNow)
+          res.data.push({
+            "Date":self.data.dateNow,
+            "List":self.data.list
+          })
+        }
+        flag = false;
+        res.data.forEach(each=>{
+          if (each.Date == (e.detail.value.slice(0, 4) + "" + (e.detail.value[5] == '0' ? e.detail.value.slice(6, 7) : e.detail.value.slice(5, 7) )+""+ e.detail.value.slice(8, 10))){
+            // console.log("对应le")
+            // console.log(each)
+            flag = true;
+            self.setData({
+              list:each.List,
+              schedule: {
+                year: e.detail.value.slice(0, 4),
+                month:tmonth,
+                day:e.detail.value.slice(8,10),
+                weekDay:wd
+              },
+              dateNow: (e.detail.value.slice(0, 4) + "" + (e.detail.value[5] == '0' ? e.detail.value.slice(6, 7) : e.detail.value.slice(5, 7)) + "" + e.detail.value.slice(8, 10))
+            })
+            wx.setStorage({
+              key: 'Schedule',
+              data: res.data,
+            })
+          }
+        })
+        if(!flag){// 原本缓存里没有现在的日期
+          // res.data.forEach(each => {
+          //   if (each.Date == self.data.dateNow) {// 找之前的
+          //     each.List = self.data.list;
+          //     console.log(self.data.list)
+          //     flag=true;
+          //     ///// 找到了之前的
+          //     wx.setStorage({
+          //       key: 'Schedule',
+          //       data: res.data,
+          //     })
+          //   }
+          // })
+          // if (!flag) {// 没找到之前的
+          //   res.data.push({
+          //     "Date": self.data.dateNow,
+          //     "List": self.data.list
+          //   })
+            wx.setStorage({
+              key: 'Schedule',
+              data: res.data,
+            })
+          // }
+          self.setData({
+            list: [{ t: null, color: "#eeeeee", choosen: false, hasTask: false }, { t: null, color: "#eeeeee", choosen: false, hasTask: false }, { t: null, color: "#eeeeee", choosen: false, hasTask: false }, { t: null, color: "#eeeeee", choosen: false, hasTask: false }, { t: null, color: "#eeeeee", choosen: false, hasTask: false }, { t: null, color: "#eeeeee", choosen: false, hasTask: false }, { t: null, color: "#eeeeee", choosen: false, hasTask: false }, { t: null, color: "#eeeeee", choosen: false, hasTask: false }, { t: null, color: "#eeeeee", choosen: false, hasTask: false }, { t: null, color: "#eeeeee", choosen: false, hasTask: false }, { t: null, color: "#eeeeee", choosen: false, hasTask: false }, { t: null, color: "#eeeeee", choosen: false, hasTask: false }, { t: null, color: "#eeeeee", choosen: false, hasTask: false }, { t: null, color: "#eeeeee", choosen: false, hasTask: false }, { t: null, color: "#eeeeee", choosen: false, hasTask: false }, { t: null, color: "#eeeeee", choosen: false, hasTask: false }, { t: null, color: "#eeeeee", choosen: false, hasTask: false }, { t: null, color: "#eeeeee", choosen: false, hasTask: false }, { t: null, color: "#eeeeee", choosen: false, hasTask: false }, { t: null, color: "#eeeeee", choosen: false, hasTask: false }, { t: null, color: "#eeeeee", choosen: false, hasTask: false }, { t: null, color: "#eeeeee", choosen: false, hasTask: false }, { t: null, color: "#eeeeee", choosen: false, hasTask: false }, { t: null, color: "#eeeeee", choosen: false, hasTask: false }],
+            schedule: {
+              year: e.detail.value.slice(0, 4),
+              month: tmonth,
+              day: e.detail.value.slice(8, 10),
+              weekDay: wd
+            },
+            dateNow: (e.detail.value.slice(0, 4) + "" + (e.detail.value[5] == '0' ? e.detail.value.slice(6, 7) : e.detail.value.slice(5, 7)) + "" + e.detail.value.slice(8, 10))
+          })
+        }
+      },
+      fail:function(){
+        
+        self.setData({
+          list: [{ t: null, color: "#eeeeee", choosen: false, hasTask: false }, { t: null, color: "#eeeeee", choosen: false, hasTask: false }, { t: null, color: "#eeeeee", choosen: false, hasTask: false }, { t: null, color: "#eeeeee", choosen: false, hasTask: false }, { t: null, color: "#eeeeee", choosen: false, hasTask: false }, { t: null, color: "#eeeeee", choosen: false, hasTask: false }, { t: null, color: "#eeeeee", choosen: false, hasTask: false }, { t: null, color: "#eeeeee", choosen: false, hasTask: false }, { t: null, color: "#eeeeee", choosen: false, hasTask: false }, { t: null, color: "#eeeeee", choosen: false, hasTask: false }, { t: null, color: "#eeeeee", choosen: false, hasTask: false }, { t: null, color: "#eeeeee", choosen: false, hasTask: false }, { t: null, color: "#eeeeee", choosen: false, hasTask: false }, { t: null, color: "#eeeeee", choosen: false, hasTask: false }, { t: null, color: "#eeeeee", choosen: false, hasTask: false }, { t: null, color: "#eeeeee", choosen: false, hasTask: false }, { t: null, color: "#eeeeee", choosen: false, hasTask: false }, { t: null, color: "#eeeeee", choosen: false, hasTask: false }, { t: null, color: "#eeeeee", choosen: false, hasTask: false }, { t: null, color: "#eeeeee", choosen: false, hasTask: false }, { t: null, color: "#eeeeee", choosen: false, hasTask: false }, { t: null, color: "#eeeeee", choosen: false, hasTask: false }, { t: null, color: "#eeeeee", choosen: false, hasTask: false }, { t: null, color: "#eeeeee", choosen: false, hasTask: false }],
+          schedule: {
+            year: e.detail.value.slice(0, 4),
+            month: tmonth,
+            day: e.detail.value.slice(8, 10),
+            weekDay: wd
+          },
+          dateNow: (e.detail.value.slice(0, 4) + "" + (e.detail.value[5] == '0' ? e.detail.value.slice(6, 7) : e.detail.value.slice(5, 7)) + "" + e.detail.value.slice(8, 10))
+        })
+        // console.log("现在日期ne" + self.data.dateNow)
       }
     })
   },
@@ -194,7 +348,7 @@ Page({
   },
   formSubmit:function(e){
     var i = this.data.chooseIndex;
-    console.log(e)
+    // console.log(e)
     this.setData({
       [`list[${i}].t`]: { Title: e.detail.value.title,Content:e.detail.value.content},
       [`list[${i}].hasTask`]: true,
@@ -212,7 +366,7 @@ Page({
   },
   addTask:function(e){
     var i = e.currentTarget.dataset.index;
-    
+    var self = this;
     if(this.data.list[i].hasTask){
       var self = this;
       wx.showModal({ 
@@ -254,6 +408,10 @@ Page({
         previourIndex: e.currentTarget.dataset.index
       })
     }
+    // wx.setStorage({
+    //   key: 'Schedule',
+    //   data: [ { "Date": self.data.dateNow, 'List': self.data.list }],
+    // })
   },
   addMyTask:function(e){
 
@@ -312,7 +470,7 @@ Page({
         [`list[${i}].color`]:app.globalData.color.Blue,
         [`list[${i}].choosen`]: false,
       })
-      console.log(this.data.list[i])
+      // console.log(this.data.list[i])
     }else{
       var self = this;
       var dataSet = e.currentTarget.dataset;
