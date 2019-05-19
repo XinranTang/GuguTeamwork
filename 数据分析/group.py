34495b8775e815a5d967d074b145cfa282ff507b
Aka <784595821@qq.com>
2019-05-19 20:54:42 +0800

    增加了数据分析的python代码

---
+++ 数据分析/group.py
@@ -0,0 +1,43 @@
+import sqlite3 as db
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+plt.rcParams['font.sans-serif']=['SimHei'] #用来正常显示中文标签
+plt.rcParams['axes.unicode_minus']=False #用来正常显示负号
+
+def readFronSqllite(db_path,exectCmd):
+    conn = db.connect(db_path)  # 该 API 打开一个到 SQLite 数据库文件 database 的链接，如果数据库成功打开，则返回一个连接对象
+    cursor=conn.cursor()        # 创建一个 cursor，将在 Python 数据库编程中用到。
+    conn.row_factory=db.Row     # 可访问列信息
+    cursor.execute(exectCmd)    #该例程执行一个 SQL 语句
+    rows=cursor.fetchall()      #该例程获取查询结果集中所有（剩余）的行，返回一个列表。当没有可用的行时，则返回一个空的列表。
+    return rows
+
+
+# 解析ARPA 单帧信息
+def readfromDB(content):
+    infor = []
+    subContent=content.split(',')
+    infor.append(subContent)
+    return infor
+if __name__=="__main__":
+    # openID = input("OpenId:")
+    # 这里改一下绝对路径
+    results=readFronSqllite('C://Users//gmf//Desktop//微信小程序大赛//咕咕TeamWork//GuguTeamwork//GuguTeamwork//SQLite3//UserInfor.db'," select taskID, done,not_done,date_rate from user_task where userID = '"+'testopenid'+"'")
+    result = []
+    for each in list(results):
+        print(each)
+        li = list(each)
+        result.append(li)
+    i=1
+    for each in result:
+
+        plt.barh(i,int(each[1]),color='#88caed')
+        plt.barh(i,int(each[2]),color='#f86e3d')
+        plt.text(0,i,each[0])
+        i=i+1
+    plt.xticks([])
+    plt.yticks([])
+    plt.savefig("3.jpg")
+    plt.show()
--- 数据分析/personal.py
+++ 数据分析/personal.py
@@ -1,7 +1,14 @@
 import sqlite3 as db
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+plt.rcParams['font.sans-serif']=['SimHei'] #用来正常显示中文标签
+plt.rcParams['axes.unicode_minus']=False #用来正常显示负号
+
 def readFronSqllite(db_path,exectCmd):
     conn = db.connect(db_path)  # 该 API 打开一个到 SQLite 数据库文件 database 的链接，如果数据库成功打开，则返回一个连接对象
-    cursor=conn.cursor()        # 该例程创建一个 cursor，将在 Python 数据库编程中用到。
+    cursor=conn.cursor()        # 创建一个 cursor，将在 Python 数据库编程中用到。
     conn.row_factory=db.Row     # 可访问列信息
     cursor.execute(exectCmd)    #该例程执行一个 SQL 语句
     rows=cursor.fetchall()      #该例程获取查询结果集中所有（剩余）的行，返回一个列表。当没有可用的行时，则返回一个空的列表。
@@ -15,15 +22,16 @@ def readfromDB(content):
     infor.append(subContent)
     return infor
 if __name__=="__main__":
-    openID = input("OpenId:")
+    # openID = input("OpenId:")
     # 这里改一下绝对路径
-    rows=readFronSqllite('C://Users//gmf//Desktop//微信小程序大赛//咕咕TeamWork//GuguTeamwork//GuguTeamwork//SQLite3//UserInfor.db'," select * from user_infor where ID = '"+openID+"'")
-    readLines=len(rows)
-    lineIndex=0
-    infor = []
-    while lineIndex<readLines:
-        row=rows[lineIndex] # 获取某一行的数据,类型是tuple
-        content=",".join(row) #tuple转字符串
-        infor = readfromDB(content) # 解析ARPA数据
-        lineIndex+=1
-    str = ""
+    results=readFronSqllite('C://Users//gmf//Desktop//微信小程序大赛//咕咕TeamWork//GuguTeamwork//GuguTeamwork//SQLite3//UserInfor.db'," select totalmanage,totaljoin,goingon from user_infor where ID = '"+'testopenid'+"'")
+    result = list(results[0])
+    plt.pie(result[:2],colors=['#88caed','#f86e3d'],autopct='%1.1f%%',labels = [u'我管理的任务', u'我加入的任务'])
+    plt.title(u"任务分布情况")
+    plt.savefig("1.jpg")
+    plt.show()
+    result.append(result[0]+result[1]-result[2])
+    plt.pie(result[2:], colors=['#88caed', '#f86e3d'], autopct='%1.1f%%', labels=[u'未完成任务', u'已完成任务'])
+    plt.title(u"任务完成情况")
+    plt.savefig("2.jpg")
+    plt.show()
